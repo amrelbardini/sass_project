59c530079e548963839daf1fd020352f23d07e27
Amr Elbardini <49646105+amrelbardini@users.noreply.github.com>
2021-08-09 07:06:18 +0200

    sidebar-pagination added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+
+    $(".meal .meal-info .text .readmore").on("click", function(){
+        //read more on click toggle class hidden on element
+       $(this).siblings("p.hidden").toggle("hidden");
+       //change read more to read less if it's value is read more
+       if($(this).html().toLowerCase()==="read more"){
+        $(this).html("Read Less");
+       }else{
+        $(this).html("Read More");
+       };
+      
+    });
     
 });
